7554a0e008778e2b6229b44f2fffe39dc67ecde2
sharmamayank94 <sharmamayank941@gmail.com>
2021-04-29 15:35:44 +0530

    right;

--- index2.js
+++ index2.js
@@ -1,12 +1,14 @@
 function bringBack(obj)
 {
-	var delay = Math.random();
 
-	obj.style.transitionDelay = delay + "s";
-	obj.style.transitionDuration = "1.5s";
-	// obj.style.transform = "translateX(0px)";
-	obj.style.width = "90px";
-	obj.style.height = "90px";
+	for(var i = 0; i < obj.length; i++)
+	{
+		var delay = Math.random();		
+		obj[i].style.transitionDelay = delay + "s";
+		obj[i].style.transitionDuration = "1.5s";
+		obj[i].style.width = "90px";
+		obj[i].style.height = "90px";
+	}
 }
 var colors = ["red", "green", "blue", "yellow", "orange"]
 function borderColor(obj)
@@ -14,30 +16,46 @@ function borderColor(obj)
 	for(var i = 0; i < obj.length; i++)
 	{
 		obj[i].style.borderColor = colors[i];
+		
+	}
+}
+
+function arrangeSidebar(obj){
+	for(var i = 0; i < obj.length; i++)
+	{
+		obj[i].style.zIndex = i;
 	}
 }
 
 var circularCards = document.querySelectorAll(".nav-options-card-holder"); 
-
-for(var i = 0; i < circularCards.length; i++)
-{
-	bringBack(circularCards[i]);
-}
+var sidebarCards = document.querySelectorAll(".sidebar_card");
+bringBack(circularCards);
 
 borderColor(circularCards);
+borderColor(sidebarCards);
 
 function aboutFlash()
 {
 	var obj = document.querySelector(".aboutflash");
+	var nameBottom = document.querySelector(".name_bottom");
+	var navSectionHolder = document.querySelector(".nav_section_holder");
+	var sidebar = document.querySelector(".sidebar");
 	obj.style.transitionDuration = "0.5s";
 	obj.style.width = "100%";
-
+	
+	
+	
 	setTimeout(()=>{
-		var obj = document.querySelector(".aboutflash");
+		sidebar.style.display = "flex";
+
+		navSectionHolder.style.display = "none";
+
 		obj.style.left = "0px";
 		obj.style.width = "0px";
-		
-		
+
+		nameBottom.style.width = "70%";
+		nameBottom.style.fontSize = "20px";
+		nameBottom.innerHTML = "I was born and raised in Bhopal. Currently I am pursuing my Bachelor of Technology in Computer Science and Engineering from University Institute of Technology, RGPV, Bhopal. I started coding in C++ when I was in 8th standard, although I took a break in 9th standard and started doing a little web development. I took a break from it too in 10th standard and started to focus solely on my studies, but my passion for programming was lit again as I stepped in 2nd year of my college.";		
 	}, 500);
 
 	setTimeout(()=>{
@@ -45,4 +63,9 @@ function aboutFlash()
 		obj.style.left = null;
 		
 	}, 1000);
+}
+
+
+window.onScroll = ()=>{
+	
 }
